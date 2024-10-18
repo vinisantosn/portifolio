19cc3fa8a0b9bac29494886db862688eb5bc1822
Vinicius Santos <vinisantoosk@gmail.com>
2024-10-18 10:45:33 -0300

    feat: componentes para texto

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/vinisantosn.png" alt="" />
+    <Titulo fontSize={20}> Vinícius Santos</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam aperiam
+      at beatae nam animi tempora nostrum modi molestias deleniti provident
+      repellendus autem qui dolor ratione, eligendi impedit omnis voluptas
+      assumenda.
+    </Paragrafo>
+  </section>
+)
+export default Sobre
