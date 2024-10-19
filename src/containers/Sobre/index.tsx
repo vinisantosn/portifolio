8ba714f287ab89a281d7407e454bdfe5af3a8d56
Vinicius Santos <vinisantoosk@gmail.com>
2024-10-19 14:57:10 -0300

    feat: adicionando sobre

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -10,6 +11,10 @@ const Sobre = () => (
       repellendus autem qui dolor ratione, eligendi impedit omnis voluptas
       assumenda.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=vinisantosn&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=vinisantosn&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 export default Sobre
--- src/styles.tsx
+++ src/styles.tsx
@@ -8,6 +8,9 @@ const EstiloGlobal = createGlobalStyle`
 }
   body {
     padding-top: 80px;
+    @media ( max-width: 768px ) {
+      padding-top: 16px;
+    }
   }
 `
 export default EstiloGlobal
@@ -19,6 +22,12 @@ export const Container = styled.div`
   display: grid;
   grid-template-columns: 128px auto;
   column-gap: 56px;
+
+  @media (max-width: 768px) {
+    max-width: 80%;
+    display: block;
+  }
+
   img {
     max-width: 100%;
   }
