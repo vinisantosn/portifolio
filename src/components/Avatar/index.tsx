ab9280d2a230852fd86f5dde52f63f3305ec33ba
Vinícius Santos <vinisantoosk@gmail.com>
2024-10-19 16:14:28 -0300

    feat: estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+const Avatar = () => (
+  <FotoPerfil src="https://github.com/vinisantosn.png" alt="" />
+)
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/vinisantosn.png" alt="" />
-    <Titulo fontSize={20}> Vinícius Santos</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}> Vinícius Santos</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        vinisantosn
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro da computação
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
